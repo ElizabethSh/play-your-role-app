77fdb401223029c0c0070a12fcddccb1bec360ea
Lisa <liza.shoshkina@gmail.com>
2025-05-26 13:40:28 +0200

    Add routing

---
+++ src/components/app/App.tsx
@@ -0,0 +1,29 @@
+import React, { Suspense } from "react";
+import { Route, Routes } from "react-router";
+
+import { AppRoute } from "../../settings";
+import Layout from "../layout";
+import Loader from "../loader";
+
+const NotFoundPage = React.lazy(() => import("../pages/not-found"));
+const HomePage = React.lazy(() => import("../pages/home"));
+
+const App = () => {
+  return (
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path={AppRoute.Root} element={<HomePage />} />
+          <Route path={AppRoute.Characters}>
+            <Route path=":id" />
+            <Route path="new" />
+            {/* <Route path="*" element={<NotFoundPage />} />   */}
+          </Route>
+          <Route path={AppRoute.NotFound} element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </Suspense>
+  );
+};
+
+export default App;
