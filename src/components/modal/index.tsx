12f6b00573c0512f4b73657040473b9d97849c09
Lisa <liza.shoshkina@gmail.com>
2025-06-10 11:44:20 +0200

    Add styles for the <Modal>

--- src/components/modal/index.tsx
+++ src/components/modal/index.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 
-type MenuModalProps = {
+import "./modal.scss";
+
+type ModalProps = {
   dialogRef: React.RefObject<HTMLDialogElement | null>;
   children: React.ReactNode;
 };
 
-const Modal: React.FC<MenuModalProps> = ({ children, dialogRef }) => {
-  return <dialog ref={dialogRef}>{children}</dialog>;
+const Modal: React.FC<ModalProps> = ({ children, dialogRef }) => {
+  return (
+    <dialog className="modal" ref={dialogRef}>
+      {children}
+    </dialog>
+  );
 };
 
 export default Modal;
