580f0627d4ff720b17bd2dcba7906dfd8b8b1c33
Lisa <liza.shoshkina@gmail.com>
2025-07-17 12:48:55 +0200

    Oprimization: memoize getValue and setValue

--- src/hooks/use-indexed-db.tsx
+++ src/hooks/use-indexed-db.tsx
@@ -1,3 +1,5 @@
+import { useCallback } from "react";
+
 import { useNotifications } from "@context/notifications";
 import { ERROR_NOTIFICATIONS, SUCCESS_NOTIFICATIONS } from "@settings";
 import { get, set } from "idb-keyval";
@@ -7,32 +9,35 @@ import { Character } from "types/character";
 export const useIndexedDB = (key: string) => {
   const { addNotification } = useNotifications();
 
-  const getValue = async (): Promise<Character[] | undefined> => {
+  const getValue = useCallback(async (): Promise<Character[] | undefined> => {
     try {
       const item = await get(key);
       return item ? (item as Character[]) : [];
     } catch {
       return undefined;
     }
-  };
+  }, [key]);
 
-  const setValue = async (
-    value: Character[],
-    action: keyof typeof SUCCESS_NOTIFICATIONS
-  ): Promise<void> => {
-    try {
-      await set(key, value);
-      addNotification({
-        title: "success",
-        description: SUCCESS_NOTIFICATIONS[action],
-      });
-    } catch {
-      addNotification({
-        title: "error",
-        description: ERROR_NOTIFICATIONS[action],
-      });
-    }
-  };
+  const setValue = useCallback(
+    async (
+      value: Character[],
+      action: keyof typeof SUCCESS_NOTIFICATIONS
+    ): Promise<void> => {
+      try {
+        await set(key, value);
+        addNotification({
+          title: "success",
+          description: SUCCESS_NOTIFICATIONS[action],
+        });
+      } catch {
+        addNotification({
+          title: "error",
+          description: ERROR_NOTIFICATIONS[action],
+        });
+      }
+    },
+    [key, addNotification]
+  );
 
   return { getValue, setValue };
 };
