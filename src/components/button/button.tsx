93543985d56d18fad6eccbb113416c990be4fd9f
Lisa <liza.shoshkina@gmail.com>
2025-07-22 13:46:37 +0200

    Refactoring: extends MUIButtonProps

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,32 +1,27 @@
-import MUIButton from "@mui/material/Button";
+import MUIButton, { ButtonProps as MUIButtonProps } from "@mui/material/Button";
 
-import "index.scss";
+import "./button.scss";
 
-type ButtonProps = {
-  type?: "button" | "submit" | "reset";
+type ButtonProps = MUIButtonProps & {
   label: string;
   variant: "contained" | "outlined";
-  className?: string;
-  onClick?: React.MouseEventHandler<HTMLButtonElement>;
   modification: "primary" | "confirm" | "danger";
-  disabled?: boolean;
 };
 
 const Button: React.FC<ButtonProps> = ({
   variant,
   label,
   type,
-  className,
-  onClick,
   modification,
   disabled = false,
+  ...buttonProps
 }) => {
   return (
     <MUIButton
+      {...buttonProps}
       variant={variant}
-      className={`button button-${modification} ${className || ""}`}
+      className={`button-custom button-${modification}`}
       type={type}
-      onClick={onClick}
       disabled={disabled}
     >
       {label}
