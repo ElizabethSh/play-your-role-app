097e2d3633d74656aa670bd8474d7205b2e71113
Lisa <liza.shoshkina@gmail.com>
2025-07-22 15:34:32 +0200

    Refactor buildCoreAbilities function

--- src/utils.ts
+++ src/utils.ts
@@ -1,30 +1,21 @@
 import { FormFields } from "@pages/characters/form";
 
+import { Ability } from "types/character";
+
+const getAbility = (ability: number | ""): Ability => {
+  return {
+    score: Number(ability),
+    modifier: Math.floor((Number(ability) - 10) / 2),
+  };
+};
+
 export const buildCoreAbilities = (data: FormFields) => {
   return {
-    strength: {
-      score: Number(data.strength),
-      modifier: Math.floor((Number(data.strength) - 10) / 2),
-    },
-    dexterity: {
-      score: Number(data.dexterity),
-      modifier: Math.floor((Number(data.dexterity) - 10) / 2),
-    },
-    constitution: {
-      score: Number(data.constitution),
-      modifier: Math.floor((Number(data.constitution) - 10) / 2),
-    },
-    intelligence: {
-      score: Number(data.intelligence),
-      modifier: Math.floor((Number(data.intelligence) - 10) / 2),
-    },
-    wisdom: {
-      score: Number(data.wisdom),
-      modifier: Math.floor((Number(data.wisdom) - 10) / 2),
-    },
-    charisma: {
-      score: Number(data.charisma),
-      modifier: Math.floor((Number(data.charisma) - 10) / 2),
-    },
+    strength: getAbility(data.strength),
+    dexterity: getAbility(data.dexterity),
+    constitution: getAbility(data.constitution),
+    intelligence: getAbility(data.intelligence),
+    wisdom: getAbility(data.wisdom),
+    charisma: getAbility(data.charisma),
   };
 };
